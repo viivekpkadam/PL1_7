d948ba961b933f24f814c18c929ae9f2a9908b2e
viivekpkadam <viivek.p.kadam@gmail.com>
2026-03-26 12:30:01 +0530

    Add files via upload

---
+++ Playwright/playwright.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./src/tests",
+  retries: 0,
+  timeout: 180000,
+  navigationTimeout: 60000,
+  expect: {
+    timeout: 30000,
+  },
+  use: {
+    baseURL: "https://yakshahrm.makemylabs.in/orangehrm-5.7",
+    trace: "on",
+    headless: false,
+    screenshot: "only-on-failure",
+    video: "retry-with-video",
+    navigationTimeout: 60000,
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1520, height: 1080 },
+        launchOptions: {
+          args: ["--disable-web-security"],
+        },
+      },
+    },
+  ],
+
+  reporter: [
+    ["list"], // Default console output
+    [
+      "./jest/PlaywrightCustomReporter.js",
+      { customOption: "value" }, // Optional configuration for the custom reporter
+    ],
+  ],
+});
+
